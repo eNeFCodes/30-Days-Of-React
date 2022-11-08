8f36ee9923a1d9a9a2a7078f8196e6c79fed1bb0
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-11-08 13:43:29 +0800

    day9: add loading data simulation

--- 30dayapp/src/components/day9/index.js
+++ 30dayapp/src/components/day9/index.js
@@ -1,19 +1,37 @@
 import './index.css';
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BackgroundChange from "./BackgroundChange";
 import { SeasonItemsMock, DayTimeItemsMock } from "./BackgroundChangeMock";
 
 const Day9 = () => {
+    const [isLoading, setIsLoading] = useState(() => true);
+
+    useEffect(() => {
+        const timer = setTimeout(function () {
+            setIsLoading(false);
+        }, 3000);
+        return () => clearTimeout(timer);
+    });
+
     return (
         <div className="day9-index-main">
-            <BackgroundChange
-                title={'Season Change'}
-                items={SeasonItemsMock}
-            />
-            <BackgroundChange
-                title={'Day Change'}
-                items={DayTimeItemsMock}
-            />
+            {isLoading ?
+                <div className='day9-index-loading-container'>
+                    <span className='day9-index-title'>30 Days of React</span>
+                    <span className='day9-index-loading'>Loading data... Please wait.</span>
+                </div>
+                :
+                <React.Fragment>
+                    <BackgroundChange
+                        title={'Season Change'}
+                        items={SeasonItemsMock}
+                    />
+                    <BackgroundChange
+                        title={'Day Change'}
+                        items={DayTimeItemsMock}
+                    />
+                </React.Fragment>
+            }
         </div>
     );
 };
