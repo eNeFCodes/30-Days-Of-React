e7cf73134bade6d58fe13b1ae1fc1d19d0e0edb2
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-11-16 10:27:03 +0800

    day19: finish code challenge

--- 30dayapp/src/components/day19/Context.js
+++ 30dayapp/src/components/day19/Context.js
@@ -1,14 +1,95 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 export const Day19Context = React.createContext();
 
 export const Day19Provider = ({ children }) => {
-    const [day19Data, setDay19Data] = useState(() => {
-        return {};
-    });
+    const [day19Data, setDay19Data] = useState({});
+    const [weightTotalAve, setWeightTotalAve] = useState(0);
+    const [lifespanTotalAve, setLifespanTotalAve] = useState(0);
+    const [catImageURL, setCatImageURL] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchCats = async () => {
+        try {
+            const result = await axios.get('https://api.thecatapi.com/v1/breeds');
+            const { data } = result;
+
+            let weightTotal = 0;
+            let lifespanTotal = 0;
+
+            for (const d of data) {
+                let { weight } = d;
+                let { metric } = weight;
+                let metricRange = metric.split('-');
+                let metricRangeAve = 0;
+                if (metricRange.lenth > 1) {
+                    metricRangeAve = (parseFloat(metricRange[0]) + parseFloat(metricRange[1])) / 2;
+                } else {
+                    metricRangeAve = parseFloat(metricRange[0]);
+                }
+                weightTotal += metricRangeAve;
+
+                let { life_span } = d;
+                let lifeSpanRange = life_span.split('-');
+                let leftSpanRangeAve = 0;
+                if (lifeSpanRange.lenth > 1) {
+                    leftSpanRangeAve = (parseFloat(lifeSpanRange[0]) + parseFloat(lifeSpanRange[1])) / 2;
+                } else {
+                    leftSpanRangeAve = parseFloat(lifeSpanRange[0]);
+                }
+                lifespanTotal += leftSpanRangeAve;
+            }
+
+            let weithTotalAve = weightTotal / data.length;
+            setWeightTotalAve(weithTotalAve.toFixed(2));
+
+            let lifespanTotalAve = lifespanTotal / data.length;
+            setLifespanTotalAve(lifespanTotalAve.toFixed(2));
+            setDay19Data(data);
+
+            const rndIndex = Math.floor(Math.random() * data.length);
+            const randData = data[rndIndex];
+            const { image } = randData;
+            setCatImageURL(image.url);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchCats();
+    }, []);
+
+    const generateCatImage = async (id) => {
+        setIsLoading(true);
+        try {
+            const fetchURL = `https://api.thecatapi.com/v1/images/search?breed_id=${id}`;
+            const result = await axios.get(fetchURL);
+            const { data } = result;
+            const { url } = data[0];
+            setCatImageURL(url);
+        } catch (error) {
+            console.error(error);
+        }
+        setIsLoading(false);
+    }
+
+    const generateRandomCatImage = () => {
+        const rndIndex = Math.floor(Math.random() * day19Data.length);
+        const randData = day19Data[rndIndex];
+        generateCatImage(randData.id);
+    }
 
     return (
-        <Day19Context.Provider value={day19Data}>
+        <Day19Context.Provider value={{
+            data: day19Data,
+            weightTotalAve,
+            lifespanTotalAve,
+            catBreeds: day19Data.length,
+            catImageURL,
+            isLoading,
+            generateRandomCatImage
+        }}>
             {children}
         </Day19Context.Provider>
     )
--- 30dayapp/src/components/day19/Day19.js
+++ 30dayapp/src/components/day19/Day19.js
@@ -1,23 +1,46 @@
-import { useState } from 'react';
+import React, { useContext } from 'react';
 import './Day19.css';
+import { Day19Context, Day19Provider } from './Context';
+const loadingImg = require('../../assets/loading.gif');
 
-const Day19 = () => {
-    const [catBreeds, setCatBreeds] = useState(0);
-    const [averageWeight, setAverageWeight] = useState(0);
-    const [averageLives, setAverageLives] = useState(0);
+const Day19Content = () => {
+    const {
+        catBreeds,
+        weightTotalAve,
+        lifespanTotalAve,
+        catImageURL,
+        isLoading,
+        generateRandomCatImage
+    } = useContext(Day19Context);
 
     return (
         <div className="day19-main">
             <div className='day19-header'>
                 <span className='day19-title'>30 DAYS OF REACT</span>
                 <span className='day19-title2'>Cats Paradise</span>
-                <span className='day19-details'>There are {`${catBreeds}`} cat breeds</span>
-                <span className='day19-details2'>On average a cat can weight about {`${averageWeight}`} Kg and lives {`${averageLives}`} years.</span>
+                <span className='day19-details'>There are <span className='day19-value'>{`${catBreeds}`} cat breeds</span></span>
+                <span className='day19-details2'>On average a cat can weight about <span className='day19-value'>{`${weightTotalAve} Kg`}</span> and lives <span className='day19-value'>{`${lifespanTotalAve} years.`}</span></span>
+                <button className='day19-button' onClick={generateRandomCatImage}>Random Cat Image</button>
             </div>
             <div className='day19-content'>
-
+                {isLoading ?
+                    <img className='day19-image' src={loadingImg} />
+                    :
+                    <div className='day19-seed'>
+                        <img className='day19-image' src={catImageURL} />
+                        <span className='day19-details'>Seed: <a href={catImageURL}>{`${catImageURL}`}</a></span>
+                    </div>
+                }
             </div>
         </div>
+    );
+};
+
+const Day19 = () => {
+    return (
+        <Day19Provider>
+            <Day19Content />
+        </Day19Provider>
     );
 };
 export default Day19;
